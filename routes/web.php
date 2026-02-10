5cdbc06fa19a064f16b5c33f148ff8ab5ec9a6e3
ayanakkabi1 <aynour200609@gmail.com>
2026-02-10 14:22:48 +0100

    YDV-16 feat: update admin dashboard route to use role-based middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\adminController;
+use App\Http\Controllers\Admin\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,3 +17,11 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+// Route::middleware(['auth:sanctum', 'is_admin'])->group(function () {
+//     Route::get('/admin/dashboard', [adminController::class, 'index'])->name('admin.dashboard');
+// });
+
+
+Route::middleware(['auth', 'role:admin'])
+    ->get('/admin/dashboard', [DashboardController::class, 'index']);
+
