14167e5bf2971bb664a10e4972ff253d4dff29ed
ayanakkabi1 <aynour200609@gmail.com>
2026-02-12 14:47:13 +0100

    YDV -1 feat: refactor Reservation model to include fillable attributes and relationships

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -1,8 +1,37 @@
 <?php
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Reservation extends Model{
-    
+class Reservation extends Model
+{
+    use HasFactory;
+
+    /**
+     * Les attributs qui peuvent être assignés en masse.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'date_reservation',
+        'heure_reservation',
+        'nombre_personnes',
+        'restaurant_id',
+        'user_id',
+        'status',
+    ];
+
+    /**
+     * Relations
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function restaurant()
+    {
+        return $this->belongsTo(Restaurant::class);
+    }
 }
